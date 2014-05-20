1e0343d94634e172b2cbd6e02ccd9126d1bea157
Balthazar Gronon <bgronon@gmail.com>
2014-05-20 19:54:40 +0200

    removing mock

--- js/ng-datetime.js
+++ js/ng-datetime.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('ngDatetime', [])
-	.directive('ngDatetime', function ($timeout) {
+	.directive('ngDatetime', function () {
 
 		return {
 			restrict: 'AE',
 			replace : true,
 			template:
-			'<div class="well rounded-left"><p ng-click="lol()">fuck</p>' +
+			'<div class="well rounded-left">' +
 				'<div class="input-append date">' +
 					'<span class="add-on">' +
 					'<i data-time-icon="icon-clock" data-date-icon="icon-calendar"></i>' +
@@ -22,12 +22,10 @@ angular.module('ngDatetime', [])
 
 				$('.well > div').datetimepicker({
 					format: 'dd/MM/yyyy hh:mm',
-					minuteStep: 5,
 					todayHighlight: true,
 					pickSeconds: false
 				})
 				.on('changeDate', function (e) {
-					console.log(e);
 					scope.$apply(function(scope) {
 						scope.value = e.localDate;
 					});
@@ -37,18 +35,12 @@ angular.module('ngDatetime', [])
 				date.setLocalDate(new Date());
 
 				scope.$watch('value', function (newVal, oldVal) {
-					console.log('watch in directive');
 					date.setLocalDate(newVal);
 				});
 
 				scope.$on('$destroy', function() {
 					$('.bootstrap-datetimepicker-widget').remove();
 				});
-
-				scope.lol = function () {
-					console.log('aldkasjl');
-						scope.value = new Date();
-				};
 
 			}
 		};
